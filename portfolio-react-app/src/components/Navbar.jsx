c9829c7c63ff4fc29cbc71fa67c092336e399148
Michaela Chang <michaela.w.chang@gmail.com>
2022-02-12 23:35:16 -0800

    created react-vite project!

---
+++ portfolio-react-app/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <div className="Navbar container-fluid">
+            Yo
+        </div>
+    )
+}
+
+export default Navbar;
